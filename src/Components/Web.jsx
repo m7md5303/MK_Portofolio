3aad2bdd09f9e9a43bdc147c51478f8b486a6f39
M7MD5303 <m7md5303@gmail.com>
2023-09-16 03:19:59 +0300

    l

--- src/Components/Web.jsx
+++ src/Components/Web.jsx
@@ -3,14 +3,14 @@ import { useState } from 'react'
 import {
     BrowserRouter as Router, Route, Link, Routes
   } from "react-router-dom";
-import webintro from 'D:/NMy portofolio/mpf/src/Images/web.jpg'
+// import webintro from 'D:/NMy portofolio/mpf/src/Images/spi.png'
 export default function Web() {
     
 
     return (
         
        <div className='web'>
-<img src={webintro} className='webintro'/>
+{/* <img src={webintro} className='webintro'/> */}
 
        </div>  
         
